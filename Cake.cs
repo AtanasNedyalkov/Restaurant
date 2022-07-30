09f25b0b2d8896ce5450e346f68b7706054ce152
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-07-30 14:32:35 +0300

    Add project files.

---
+++ Cake.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        //· Grams = 250
+
+        //· Calories = 1000
+
+        //· CakePrice = 5
+        public const double CakeGrams = 250;
+        public const double CakeCalories = 1000;
+        public const decimal CakePrice = 5.0m;
+        public Cake(string name, decimal price, double grams, double calories) : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+
+        }
+    }
+}
